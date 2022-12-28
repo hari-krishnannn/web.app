43ebad941ad5b10e68aadea1c2ebe5176f045e0f
HARI KRISHNAN <75386795+hari-krishnannn@users.noreply.github.com>
2022-12-28 14:00:22 -0800

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,35 @@
+//bot token
+var telegram_bot_id = "5981898444:AAFc72IOZ2L4yPu_AZ1FZD5gtQY9oO5_ZT4";
+//chat id
+var chat_id = 886618527;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
